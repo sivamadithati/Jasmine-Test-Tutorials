b9c834cd651b84f85e87e848a006addd0b5eb39e
sivamadithati <sivareddymadithati@gmail.com>
2016-05-19 02:19:33 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,16 @@ module.exports = function(config){
   config.set({
 
     basePath : '',
+    reporters: ['coverage'],
     preprocessors: {
-      'app/views/jasmineDirective.html': ['ng-html2js']
+      'app/views/jasmineDirective.html': ['ng-html2js'],
+      'app/**/*.js': ['coverage']
     },
     files : [
       'app/bower_components/jquery/dist/jquery.js',
       'app/bower_components/angular/angular.js',
-            'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'app/bower_components/angular-route/angular-route.js',
         'app/bower_components/ui-router/release/angular-ui-router.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
         'app/app.js',
@@ -31,13 +34,8 @@ module.exports = function(config){
             'karma-firefox-launcher',
             'karma-jasmine',
             'karma-junit-reporter',
-        'karma-ng-html2js-preprocessor'
-            ],
-
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
+        'karma-ng-html2js-preprocessor',
+        'karma-coverage'
+            ]
   });
 };
